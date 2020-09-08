c8ee94e2fc5814cfc52c003acc6c50670e13df32
mcgear <michael.gearhardt@gmail.com>
2020-09-08 15:50:40 -0600

    Adjusted to support iot starter as sub control

--- projects/common/src/lib/elements/journeys/journey-details/journey-details.component.ts
+++ projects/common/src/lib/elements/journeys/journey-details/journey-details.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { ChartDataSets, ChartType, ChartOptions } from 'chart.js';
-import { Label, Color } from 'ng2-charts';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import {
   HttpClient,
@@ -19,46 +17,10 @@ import { JourneyOption } from './../../../state/journeys/journeys.state';
 })
 export class JourneyDetailsComponent implements OnInit {
   //  Fields
-  protected httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-    }),
-  };
 
   //  Properties
-  public ChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Device 1' },
-    { data: [65, 55, 40, 59, 80, 81, 56], label: 'Device 2' },
-  ];
-
-  public ChartLabels: Label[] = [
-    'Friday',
-    'Saturday',
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-  ];
-
-  public ChartLegend = true;
-
-  public ChartType: ChartType = 'line';
-
-  public ChartPlugins: any[] = [];
-
   @Output('close')
   public CloseClicked: EventEmitter<boolean>;
-
-  public IoTChartColors: Color[];
-
-  public IoTChartOptions: ChartOptions;
-
-  public IoTDataForm: FormGroup;
-
-  public IoTDataLabels: Label[];
-
-  public IoTDataResults: ChartDataSets[];
 
   @Input('iot-data')
   public IoTData?: JourneysIoTDetails[];
@@ -66,29 +28,27 @@ export class JourneyDetailsComponent implements OnInit {
   @Input('journey')
   public Journey?: JourneyOption;
 
+  public get SupportConfigContext() {
+    return {
+      State: {
+        IoTData: this.IoTData,
+        Journey: this.Journey,
+      },
+    };
+  }
+
   //  Constructors
-  constructor(protected formBldr: FormBuilder, protected http: HttpClient) {
+  constructor() {
     this.CloseClicked = new EventEmitter();
   }
 
   public ngOnInit(): void {
-    this.IoTDataForm = this.formBldr.group({
-      deviceId: ['', Validators.required],
-      deviceType: ['', Validators.required],
-      latitude: ['', Validators.required],
-      longitude: ['', Validators.required],
-      temp: ['', Validators.required],
-      windHeading: ['', Validators.required],
-    });
-
-    this.configureCharts();
-
-    this.Journey.Details.SupportConfig = {
-      Assets: [
-        'https://mike-97d.fathym-int.com/_lcu/lcu-data-apps-lcu/wc/lcu-data-apps.lcu.js',
-      ],
-      ElementName: 'lcu-data-apps-config-manager-element',
-    };
+    // this.Journey.Details.SupportConfig = {
+    //   Assets: [
+    //     'https://mike-97d.fathym-int.com/_lcu/lcu-data-apps-lcu/wc/lcu-data-apps.lcu.js',
+    //   ],
+    //   ElementName: 'lcu-data-apps-config-manager-element',
+    // };
   }
 
   //  API Methods
@@ -100,74 +60,5 @@ export class JourneyDetailsComponent implements OnInit {
     return Object.keys(value);
   }
 
-  public SendDeviceData() {
-    const data = {
-      deviceid: this.IoTDataForm.controls.deviceId.value,
-      DeviceType: this.IoTDataForm.controls.deviceType.value,
-      Version: '1',
-      Timestamp: new Date(),
-      DeviceData: {
-        Latitude: this.IoTDataForm.controls.latitude.value,
-        Longitude: this.IoTDataForm.controls.longitude.value,
-      },
-      SensorReadings: {
-        Temperature: this.IoTDataForm.controls.temp.value,
-        WindHeading: this.IoTDataForm.controls.windHeading.value,
-      },
-    };
-
-    this.http
-      .post('/api/data-flow/iot/data-stream', data, this.httpOptions)
-      .pipe(catchError(this.handleError));
-  }
-
   //  Helpers
-  protected configureCharts() {
-    this.IoTChartOptions = { responsive: true, maintainAspectRatio: false };
-
-    this.IoTChartColors = [
-      { borderColor: 'black', backgroundColor: 'rgba(255,0,0,0.3)' },
-    ];
-
-    // this.IoTDataLabels = [
-    //   'January',
-    //   'February',
-    //   'March',
-    //   'April',
-    //   'May',
-    //   'June',
-    //   'July',
-    // ];
-    this.IoTDataLabels = !this.IoTData ? [] : Object.keys(this.IoTData[0].Data);
-
-    // this.IoTDataResults = [
-    //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'Device 1' },
-    //   { data: [65, 55, 40, 59, 80, 81, 56], label: 'Device 2' },
-    // ];
-    this.IoTDataResults = !this.IoTData
-      ? []
-      : this.IoTData.map((data) => {
-          return {
-            label: data.Name,
-            data: Object.keys(data.Data).map((dk) => {
-              return data.Data[dk];
-            }),
-          };
-        });
-  }
-
-  protected handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-      );
-    }
-    // Return an observable with a user-facing error message.
-    return throwError('Something bad happened; please try again later.');
-  }
 }

061894eca8e4c2c9a4aba5a11d377a0136f6ec53
mcgear <michael.gearhardt@gmail.com>
2021-02-10 11:39:24 -0700

    Upgrade to angular 11

--- projects/common/src/lib/elements/journeys/iot-data-chart/iot-data-chart.component.spec.ts
+++ projects/common/src/lib/elements/journeys/iot-data-chart/iot-data-chart.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { IotDataChartComponent } from './iot-data-chart.component';
 
@@ -6,7 +6,7 @@ describe('IotDataChartComponent', () => {
   let component: IotDataChartComponent;
   let fixture: ComponentFixture<IotDataChartComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ IotDataChartComponent ]
     })
--- projects/common/src/lib/elements/journeys/journey-details/journey-details.component.spec.ts
+++ projects/common/src/lib/elements/journeys/journey-details/journey-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { JourneyDetailsComponent } from './journey-details.component';
 
@@ -6,7 +6,7 @@ describe('JourneyDetailsComponent', () => {
   let component: JourneyDetailsComponent;
   let fixture: ComponentFixture<JourneyDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ JourneyDetailsComponent ]
     })
